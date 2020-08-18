e96b71fd296b23debecd760643d6c90d7cecb0ba
bobateacodes <temaybroadway@hotmail.com>
2020-08-18 00:46:48 -0500

    added working toggle for desc button

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,15 @@
 
 // more info, less info button
+function toggle() {
+  var button = document.getElementById("p-desc-b");
+  if (button.innerHTML == "more info") {
+    button.innerHTML = "less info";
+  }
+  else {
+    button.innerHTML = "more info";
+  }
+}
+
 function addInfo() {
   var x = document.getElementById("desc-info");
   if (x.style.display === "none") {
