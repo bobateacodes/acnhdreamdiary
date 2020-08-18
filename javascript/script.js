77a236e83046027a5de5dd649e8cf2f97c18098f
bobateacodes <temaybroadway@hotmail.com>
2020-08-18 00:55:26 -0500

    fixed onclick for desc button

--- javascript/script.js
+++ javascript/script.js
@@ -12,7 +12,7 @@ function toggle() {
 
 function addInfo() {
   var x = document.getElementById("desc-info");
-  if (x.style.display === "none") {
+  if (x.style.display == "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
